f9eaaa7ba5050cf658864b8cbf35ae6b0782bf55
Rameez Sajid <P15224656@LEC-ADMIN.dmu.ac.uk>
2017-11-29 11:06:42 +0000

    added navigation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-$('#nav-toggle').click(function(e) {
-  e.stopPropagation();
-  $(".menu").toggleClass('bar')
-});
-$('body').click(function(e) {
-  if ($('.menu').hasClass('bar')) {
-    $(".menu").toggleClass('bar')
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
-})
+}
