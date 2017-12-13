d28c0f782533fce335bee291c2d0abf377f058d5
Rameez Sajid <P15224656@LEC-ADMIN.dmu.ac.uk>
2017-12-13 11:35:49 +0000

    added finished script for navbar1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+ document.getElementById("my-navbar").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// this script opens the dropdown menu 
+// it also closes the dropdown menu when a link is clicked on or when clicking outside the menu
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+window.onclick = function(event) {
+ if (!event.target.matches('.dropdown-button')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+   var openDropdown = dropdowns[i];
+   if (openDropdown.classList.contains('show')) {
+    openDropdown.classList.remove('show');
+   }
   }
+ }
 }
