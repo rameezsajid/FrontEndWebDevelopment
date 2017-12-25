7fa1d03f79e4f04a59d87b5479c63cc72e397b83
Rameez Sajid <rameezsajid007@gmail.com>
2017-12-25 17:28:54 +0000

    changed scroll script

--- js/scroll.js
+++ js/scroll.js
@@ -1,53 +1,43 @@
-// I have a wrote a script to help give my website a smooth scroll effect when a button is clicked
-// I have targeted the anchor tags and gave each section in the html a id 
+// this script is used to help the website give a scroll effect 
+// and to help the user go back to top of page when a link is clicked
 
-(function(window, undefined) {
- var height = 0;
- // higher number makes scroll more smooth
- var frequency = 15;
- //speed the scroll will travel
- var speed = 700;
- // gets the a tags elements in html 
- var a_tags = document.getElementsByTagName('a');
- var href;
- for (var i = 0; i < a_tags.length; i++) {
-  href = (a_tags[i].attributes.href === undefined) ? null : a_tags[i].attributes.href.nodeValue.toString();
-  if (href !== null && href.length > 1 && href.indexOf('#') != -1) {
-   a_tags[i].onclick = function() {
-    var element,
-     href = this.attributes.href.nodeValue.toString(),
-     url = href.substr(0, href.indexOf('#')),
-     id = href.substr(href.indexOf('#') + 1);
-    if (element = document.getElementById(id)) {
-     var count = (speed - (speed % frequency)) / frequency,
-      getScrollTopDocumentAtBegin = getScrollTopDocument(),
-      gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / count;
-     if (window.history && typeof window.history.pushState == 'function')
-      for (var i = 1; i <= count; i++) {
-       (function() {
-        var position = gap * i;
-        setTimeout(function() {
-         window.scrollTo(0, position + getScrollTopDocumentAtBegin);
-        }, frequency * i);
-       })();
-      }
-     return false;
-    }
-   };
-  }
- }
+var Ymargin = 0;
+var destination = 0;
+var speed = 10; // speed in which scroll will travel
+var scrolling = null;
+var distance = 20; // distance the scroll wll travel
 
- var getScrollTopElement = function(e) {
-  var top = height * -1;
-  while (e.offsetParent != undefined && e.offsetParent != null) {
-   top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
-   e = e.offsetParent;
-  }
-  return top;
- };
+// function for scrolling when button is clicked
+function initScroll(elementId){
+	destination = document.getElementById(elementId).offsetTop;
+	
+	scrolling = setTimeout(function(){
+		initScroll(elementId);
+	}, 1);
 
- var getScrollTopDocument = function() {
-  return window.pageYOffset !== undefined ? window.pageYOffset : document.documentElement.scrollTop !== undefined ? document.documentElement.scrollTop :
-   document.body.scrollTop;
- };
-})(window);
+	Ymargin = Ymargin + speed;
+
+	if(Ymargin >= destination){
+		clearTimeout(scrolling);
+	}	
+
+	window.scroll(0, Ymargin);
+
+}
+
+window.onscroll = function(){
+	Ymargin = this.pageYOffset;	
+};
+
+// function to take user back to top of page when link clicked 
+function resetScroller(el){
+	var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+	var animator = setTimeout('resetScroller(\''+el+'\')',speed);
+	if(currentY > targetY){
+		Ymargin = currentY-distance;
+		window.scroll(0, Ymargin);
+	} else {
+		clearTimeout(animator);
+	}
+}
