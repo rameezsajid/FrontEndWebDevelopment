37baf722ea0068b1a21ef7979deed0b22a4c7037
Rameez Sajid <rameezsajid007@gmail.com>
2017-12-28 14:54:28 +0000

    added some comments in javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// this script opens the dropdown menu 
+// this script opens the first navigation bar, and shows its content 
 // function to show the content in my-navbar
 function showContent() {
  document.getElementById("my-navbar").classList.toggle("show");
